8432f6dedd0ac9f32c830216c50dc42be8d92475
Shawn Sandy <creative@shawnsandy.design>
2022-08-16 14:44:39 +0000

    feat: 🌟 add darkmode atoms

--- src/examples/Atoms.tsx
+++ src/examples/Atoms.tsx
@@ -1,28 +1,32 @@
-import { Button } from "@chakra-ui/react"
+import {atom, useRecoilState, useRecoilValue} from 'recoil'
+
+const darkModeState = atom({ key: 'darkMode', default: false})
 
 export const Atoms = () => {
-    
+
     return (
      <>
         <div>Atoms</div>
         <div><DarkModeSwitch /></div>
         <div><ToggleButton /></div>
     </>
-    
+
 )}
 
 
 const ToggleButton = () => {
+    const darkMode = useRecoilValue(darkModeState)
     return (
-        <button>Button</button>
+        <button style={{backgroundColor: darkMode ? 'black' : 'white', color: darkMode ? 'white' : 'black'}}>Button</button>
     )
 }
 
 const DarkModeSwitch = () => {
+    const [darkMode, setDarkMode] = useRecoilState(darkModeState)
     return (
         <>
         <label htmlFor="darkmodeInput">
-            <input type="checkbox" />
+            <input type="checkbox" checked={darkMode} onChange={(event) => {setDarkMode(event.currentTarget.checked)}} />
         </label>
         </>
     )
